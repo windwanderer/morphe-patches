ce4c3cc801439b49d7fd82eb9c794cd6378e4ec3
Alvin Zhu <alvin36k@gmail.com>
2026-09-18 22:25:18 +1200

    fix: revert changes

--- patches/src/main/kotlin/app/template/patches/carscanner/Fingerprints.kt
+++ patches/src/main/kotlin/app/template/patches/carscanner/Fingerprints.kt
@@ -1,5 +1,6 @@
+package app.template.patches.carscanner
+
 import app.morphe.patcher.Fingerprint
-import app.morphe.patcher.opcode
 import app.morphe.patcher.methodCall
 import com.android.tools.smali.dexlib2.AccessFlags
 
@@ -7,13 +8,11 @@ internal object PurchaseStateFingerprint : Fingerprint(
     definingClass = "Lcom/android/billingclient/api/Purchase;",
     returnType = "I",
     accessFlags = listOf(AccessFlags.PUBLIC),
-
+    strings = listOf("purchaseState"),
     filters = listOf(
         methodCall(
             definingClass = "Lorg/json/JSONObject;",
             name = "optInt",
-        ),
-        opcode("if-ne"),
-        opcode("return"),
+        )
     ),
 )
