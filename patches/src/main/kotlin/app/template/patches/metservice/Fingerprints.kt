b172f2e62e0491d1fb8c4dd943614438996a69b3
Alvin Zhu <alvin36k@gmail.com>
2026-09-19 10:28:11 +1200

    feat: update MetService patch

--- patches/src/main/kotlin/app/template/patches/metservice/Fingerprints.kt
+++ patches/src/main/kotlin/app/template/patches/metservice/Fingerprints.kt
@@ -2,11 +2,20 @@ package app.template.patches.metservice
 
 import app.morphe.patcher.Fingerprint
 import app.morphe.patcher.string
+import app.morphe.patcher.literal
+import app.morphe.patcher.methodCall
+import app.morphe.patcher.fieldAccess
+import app.morphe.patcher.opcode
+import app.morphe.patcher.InstructionLocation.MatchAfterImmediately
 import com.android.tools.smali.dexlib2.AccessFlags
+import com.android.tools.smali.dexlib2.Opcode
 
 object AppStateTFingerprint : Fingerprint(
-    definingClass = ":com/metservice/kryten/",
+    definingClass = "Lcom/metservice/kryten/AppState;",
     name = "t",
+    returnType = "Z",
+    accessFlags = listOf(AccessFlags.PUBLIC),
+
     filters = listOf(
         string("premium_ad_free_enabled")
     )
