3d7f9ed770db97ad1a2a51967502b74d0735f651
Alvin Zhu <alvin36k@gmail.com>
2026-09-19 09:59:03 +1200

    feat: update MetService patch

--- patches/src/main/kotlin/app/template/patches/metservice/MetServicePremiumPatch.kt
+++ patches/src/main/kotlin/app/template/patches/metservice/MetServicePremiumPatch.kt
@@ -5,17 +5,18 @@ import app.morphe.patcher.extensions.InstructionExtensions.replaceInstructions
 
 val MetServicePremiumPatch = bytecodePatch(
     name = "MetService Premium Unlock",
-    description = "Force BillingStatus.a() to always return true.",
+    description = "Force premium_ad_free_enabled and subscription to always be true.",
     default = true
 ) {
     execute {
-        val method = BillingStatusFingerprint.method
+        val method = OnResumeFingerprint.method
 
         method.replaceInstructions(
             0,
             """
                 const/4 v0, 0x1
-                return v0
+                const/4 v1, 0x1
+                return-void
             """
         )
     }
