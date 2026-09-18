b102cd1b245efaff8daab723b6054d7b3ada45dd
Alvin Zhu <alvin36k@gmail.com>
2026-09-18 19:55:43 +1200

    fix: update patch

--- patches/src/main/kotlin/app/template/patches/carscanner/Fingerprints.kt
+++ patches/src/main/kotlin/app/template/patches/carscanner/Fingerprints.kt
@@ -1,12 +1,19 @@
 package app.template.patches.carscanner
 
 import app.morphe.patcher.Fingerprint
+import app.morphe.patcher.methodCall
 import com.android.tools.smali.dexlib2.AccessFlags
 
 internal object PurchaseStateFingerprint : Fingerprint(
     definingClass = "Lcom/android/billingclient/api/Purchase;",
     returnType = "I",
     accessFlags = listOf(AccessFlags.PUBLIC),
+    // 不写 name，避免 R8 混淆导致匹配失败
     strings = listOf("purchaseState"),
-    literals = listOf(1, 4),
+    filters = listOf(
+        methodCall(
+            definingClass = "Lorg/json/JSONObject;",
+            name = "optInt",
+        )
+    ),
 )
