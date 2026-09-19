a119ba80a0578034c40ba6af9969008c0aba7177
Alvin Zhu <alvin36k@gmail.com>
2026-09-19 14:44:46 +1200

    feat: remove unnecessary patch for splash ad

--- patches/src/main/kotlin/app/template/patches/metservice/Fingerprints.kt
+++ patches/src/main/kotlin/app/template/patches/metservice/Fingerprints.kt
@@ -10,13 +10,6 @@ object AppStateAdFreeFingerprint : Fingerprint(
     parameters = emptyList()
 )
 
-object SplashControllerZ1Fingerprint : Fingerprint(
-    definingClass = "Lcom/metservice/kryten/ui/splash/SplashController;",
-    name = "z1",
-    returnType = "V",
-    parameters = listOf("Ljava/util/Map;")
-)
-
 object SplashPresenterTimerFingerprint : Fingerprint(
     definingClass = "Lcom/metservice/kryten/ui/splash/SplashPresenter;",
     name = "t",
--- patches/src/main/kotlin/app/template/patches/metservice/MetServicePremiumPatch.kt
+++ patches/src/main/kotlin/app/template/patches/metservice/MetServicePremiumPatch.kt
@@ -5,22 +5,16 @@ import app.morphe.patcher.extensions.InstructionExtensions.replaceInstruction
 import app.morphe.patcher.patch.bytecodePatch
 
 @Suppress("unused")
-val splashAdPatch = bytecodePatch(
-    name = "MetService Skip Splash Ad",
-    description = "Skip splash advertisement."
+val premiumPatch = bytecodePatch(
+    name = "MetService Premium",
+    description = "Enable ad-free state."
 ) {
     execute {
-
-        SplashControllerZ1Fingerprint.method.addInstructions(
+        AppStateAdFreeFingerprint.method.addInstructions(
             0,
             """
-                iget-object v0, p0, Lcom/metservice/kryten/ui/splash/SplashController;->M:Lso1;
-                invoke-interface {v0}, Lso1;->getValue()Ljava/lang/Object;
-                move-result-object v0
-                check-cast v0, Lcom/metservice/kryten/ui/splash/SplashPresenter;
-                const/4 v1, 0x0
-                invoke-virtual {v0, v1}, Lcom/metservice/kryten/ui/splash/SplashPresenter;->C(I)V
-                return-void
+                const/4 v0, 0x1
+                return v0
             """
         )
     }
@@ -39,22 +33,6 @@ val splashDelayPatch = bytecodePatch(
         SplashPresenterTimerFingerprint.method.replaceInstruction(
             timerMatch.index - 1,
             "const-wide/16 v7, 0x0"
-        )
-    }
-}
-
-@Suppress("unused")
-val premiumPatch = bytecodePatch(
-    name = "MetService Premium",
-    description = "Enable ad-free state."
-) {
-    execute {
-        AppStateAdFreeFingerprint.method.addInstructions(
-            0,
-            """
-                const/4 v0, 0x1
-                return v0
-            """
         )
     }
 }
